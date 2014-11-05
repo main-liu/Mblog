7801652affbc24e0bc3582ea2045e36ccf5975f2
Ming <ming@ming-VirtualBox.(none)>
2014-11-05 23:18:29 +0800

    add blog master page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,18 @@ class BlogsController < ApplicationController
     @blogs=Blog.all
   end
 
+  def category
+    @category= Category.find_by(name:params[:name])
+    puts @category
+    @blogs
+    if @category.nil?
+        @blogs=Blog.where(category_id:0)
+    else
+        @blogs=Blog.where(:category_id=> @category.id).order('created_at DESC')
+    end
+    render 'index'
+  end
+
   def show
     @blog=Blog.find_by(slug:params[:slug])
     redirect_to root_path, alert:'您访问点博文不存在' unless @blog
